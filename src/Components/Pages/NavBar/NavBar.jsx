b7768b52f38054cb020d65da099c8b89d36820f0
xuandinh28 <xuandinh282000@gmail.com>
2023-11-10 19:59:08 +0700

    update 10/11/2023

--- src/Components/Pages/NavBar/NavBar.jsx
+++ src/Components/Pages/NavBar/NavBar.jsx
@@ -6,10 +6,10 @@ const NavBar = () => {
         <div className=".schedule">
             <div class="row">
                 <div class="column" style={{backgroundColor: "red"}}>
-                    <p>Column 1</p>
+                    <p>Home</p>
                 </div>
                 <div class="column" >
-                    <p>Column 2</p>
+                    <p>Menu</p>
                     {/* <div class="row">
                         Row insite column 2 1
                     </div>
@@ -20,17 +20,13 @@ const NavBar = () => {
                     
 
                 <div class="column" >
-                    <p>Column 3</p>
+                    <p>About</p>
                 </div>
                 <div class="column" >
-                    <p>Column 4</p>
+                    <p>Page</p>
                 </div>
                 <div class="column" >
-                    <p>Column 5</p>
-                </div><div class="column" >
-                    <p>Column 6</p>
-                </div><div class="column" >
-                    <p>Column 7</p>
+                    <p>Contract</p>
                 </div>
             </div>
         </div>
